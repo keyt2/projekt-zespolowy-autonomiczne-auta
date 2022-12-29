2a3b72a883148f8c653b5ffef7707e129ad13578
Aleksandra Czupryńska <96470188+AleksandraC001@users.noreply.github.com>
2022-12-29 16:25:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import time
 from datetime import timedelta
 from car import Car
+#from cars import Cars
 import geopandas as gpd
 import osmnx as ox
 import networkx as nx
@@ -9,8 +10,14 @@ from pyproj import CRS
 import pandas as pd
 from request_generator import RequestGenerator
 from request import Request
-from PyQt5 import uic, QtWidgets
+from PyQt5 import uic, QtWidgets"""to bedzie do GUI"""
 
+
+#update wszystkich pozycji i statystyk samochodów naraz
+def update_all(car_list):
+    for i in range(0, 21):
+        car_list[i].update_position()
+        car_list[i].show_statistics()
 
 def main():
     city_name = "Iłża, Poland"
@@ -18,26 +25,18 @@ def main():
     nodes, edges = ox.graph_to_gdfs(graph) # nodes przechowują węzły grafu, edges krawędzie
     time_in_min = 0  # na razie zakładam, że każdy update w programie będzie oznaczał upływ 1 minuty w świecie realnym
     rg = RequestGenerator()
-    # te wszystkie auta muszą iść do jednej klasy Fleet i ich pozycja musi być aktualizowana zbiorowo;
-    # na razie ja zrobiłam tymczasowo listę z 5 pierwszych aut
+   #tworzenie floty samochodów:
     car_list = []
-    for i in range (0, 5):
+    for i in range(0, 5):
         car_list.append(Car(i, 5, 20, graph))
-    """car6 = Car(6, 5, 20, route, graph)
-    car7 = Car(7, 5, 20, route, graph)
-    car8 = Car(8, 5, 20, route, graph)
-    car9 = Car(9, 5, 20, route, graph)
-    car10 = Car(10, 5, 25, route, graph)
-    car11 = Car(11, 5, 22, route, graph)
-    car12 = Car(12, 5, 20, route, graph)
-    car13 = Car(13, 5, 21, route, graph)
-    car14 = Car(14, 5, 20, route, graph)
-    car15 = Car(15, 6, 23, route, graph)
-    car16 = Car(16, 5, 20, route, graph)
-    car17 = Car(17, 3, 20, route, graph)
-    car18 = Car(18, 5, 25, route, graph)
-    car19 = Car(19, 6, 20, route, graph)
-    car20 = Car(20, 8, 15, route, graph)"""
+    for i in range(5, 10):
+        car_list.append(Car(i, 5, 25, graph))
+    for i in range(10, 15):
+        car_list.append(Car(i, 6, 22, graph))
+    for i in range(15, 19):
+        car_list.append(Car(i, 6, 20, graph))
+    for i in range(19, 21):
+        car_list.append(Car(i, 3, 20, graph))
     while True:
         time.sleep(0.2)
         time_in_min += 1
@@ -64,23 +63,14 @@ def main():
             target_node_id = ox.nearest_nodes(graph, destination.x, destination.y)
             # wyznaczam i wyświetlam najkrótszą drogę od węzła wyjściowego do docelowego
             route = nx.shortest_path(G=graph, source=orig_node_id, target=target_node_id, weight="length")
-            ox.plot_graph_route(graph, route)
+            ox.plot_graph_route(graph, route)# pokazuje graf(?)
             # jeżeli wpłynęła jakaś prośba, to przypisuję ją do losowego auta; ale to jest rozwiązanie tymczasowe!
             # oczywiście trzeba to pozmieniać, bo w tym momencie auto może porzucić swoją dotychczasową prośbę
             # i przeteleportować się do punktu wyjściowego nowej prośby, co jest oczywiście nierealne
             random.choice(car_list).set_route(route)
         # update_position() służy do tego, żeby autko przesunęło się do przypisanej do niego drodze;
         # show_statistics() pokazuje, gdzie się obecnie znajduje
-        car_list[0].update_position()
-        car_list[0].show_statistics()
-        car_list[1].update_position()
-        car_list[1].show_statistics()
-        car_list[2].update_position()
-        car_list[2].show_statistics()
-        car_list[3].update_position()
-        car_list[3].show_statistics()
-        car_list[4].update_position()
-        car_list[4].show_statistics()
+        update_all(car_list)
         if time_in_min == 400:
             break
 
